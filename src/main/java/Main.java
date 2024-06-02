eb39ed49360d7ae02371ba20b58387584c817a2b
hafihaf123 <misobuchta@gmail.com>
2024-06-02 21:03:39 +0200

    added sheet creation to main, logic upgrade

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 package main.java;
 
+import main.java.char_sheet.CharSheetCreator;
 import main.java.creature.character.Character;
 import main.java.creature.character.CharacterManager;
 import main.java.creature.character.character_class.CharacterClassFactory;
 import main.java.creature.character.race.RaceFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -20,5 +24,19 @@ public class Main {
 		Character character = characterManager.createCharacterFromInput();
 		
 		System.out.println(STR."\n\{character}");
+
+		System.out.println("\nDo you want to create a character sheet? (Y/n)");
+		if (!Objects.equals(scanner.next(), "n")) {
+            try {
+                new CharSheetCreator(
+                        new File("/home/user1/IdeaProjects/dnd_java/documents/CharSheetPDF.pdf"),
+                        character
+                ).createCharSheet(
+                        new File(STR."/home/user1/IdeaProjects/dnd_java/documents/char_sheets/\{character.getName().toLowerCase().replace(' ', '_')}.pdf")
+                );
+            } catch (IOException e) {
+				System.out.println("The creation failed.");
+            }
+        }
 	}
 }
