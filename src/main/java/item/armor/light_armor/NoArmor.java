b43383675257dc1bf0e4a5d3b7922aad5eb06ee0
hafihaf123 <129305100+hafihaf123@users.noreply.github.com>
2024-06-03 23:03:41 +0200

    added base NoArmor class

---
+++ src/main/java/item/armor/light_armor/NoArmor.java
@@ -0,0 +1,17 @@
+package main.java.item.armor.light_armor;
+
+import main.java.item.armor.Armor;
+import main.java.item.armor.ArmorCategory;
+import main.java.units.coin.Coin;
+import main.java.units.coin.CoinUnits;
+
+public class NoArmor extends Armor {
+	public NoArmor() {
+		this.name = "No armor";
+		this.armorCategory = ArmorCategory.LIGHT_ARMOR;
+		this.cost = new Coin(CoinUnits.CP, 0);
+		this.baseArmorClass = 10;
+		this.stealthDisadvantage = false;
+		this.weight = 0;
+	}
+}
