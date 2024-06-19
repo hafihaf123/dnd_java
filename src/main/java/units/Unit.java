ba3b690806f2804b2ed20a0c82d1e934804f0cf9
hafihaf123 <misobuchta@gmail.com>
2024-06-19 14:16:30 +0200

    upgraded functionality

--- src/main/java/units/Unit.java
+++ src/main/java/units/Unit.java
@@ -1,22 +1,42 @@
 package main.java.units;
 
 public abstract class Unit <T extends EnumUnit> {
-	private double amount;
+	private double baseAmount;
 	
 	public Unit(T enumUnit, double amount) {
-		this.amount = amount * enumUnit.getBasicUnit();
+		this.baseAmount = amount * enumUnit.getBasicUnit();
 	}
-	
+
+	public double getBaseAmount() {
+		return baseAmount;
+	}
+
 	public double getAmount(T enumUnit) {
-		return amount /  enumUnit.getBasicUnit();
+		return baseAmount /  enumUnit.getBasicUnit();
 	}
-	
+
+	public void setBaseAmount(double baseAmount) {
+		this.baseAmount = baseAmount;
+	}
+
 	public void setAmount(T enumUnit, double amount) {
-		this.amount = amount * enumUnit.getBasicUnit();
+		this.baseAmount = amount * enumUnit.getBasicUnit();
+	}
+
+	public void setAmount(Unit<T> unit) {
+		this.setBaseAmount(unit.getBaseAmount());
+	}
+
+	public void addBaseAmount(double baseAmount) {
+		this.baseAmount += baseAmount;
 	}
 	
 	public void addAmount(T enumUnit, double amount) {
-		this.amount += amount * enumUnit.getBasicUnit();
+		this.baseAmount += amount * enumUnit.getBasicUnit();
+	}
+
+	public void addAmount(Unit<T> unit) {
+		this.baseAmount += unit.getBaseAmount();
 	}
 	
 	public String toString(T enumUnit) {
